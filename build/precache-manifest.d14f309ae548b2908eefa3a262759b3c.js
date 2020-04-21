b9dc419bfa35dec701e6ec08e8939e9828795b14
test <stfen@stfen.local>
2020-04-21 10:33:18 +0800

    第一次把前端react初始化文件build放到node里

---
+++ build/precache-manifest.d14f309ae548b2908eefa3a262759b3c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ff009b9b35d771cfc46291552fba76c6",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e22c94977d6bd2a34ec5",
+    "url": "/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "c894e8a8e01d6a599ce8",
+    "url": "/static/js/2.07a05dc4.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.07a05dc4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e22c94977d6bd2a34ec5",
+    "url": "/static/js/main.4a6b0373.chunk.js"
+  },
+  {
+    "revision": "0f7e17235ce2106ccee1",
+    "url": "/static/js/runtime-main.549be430.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
